98a7d4a7b6c30767c521a7cad970aa53b182b174
anil <anilsai029@gmail.com>
2022-12-07 21:00:35 +0530

    ingested_data_dir corrected

--- src/backorder/config/pipeline/training_configuration_manager.py
+++ src/backorder/config/pipeline/training_configuration_manager.py
@@ -25,11 +25,12 @@ class TrainingConfigurationManager:
             feature_store_merged_filePath = Path(os.path.join(feature_store_dir,
                                                                 DATA_INGESTION_FEATURE_STORE_MERGED_DATA_DIR_NAME,
                                                                 DATA_INGESTION_FEATURE_STORE_FILENAME))
-            ingested_data_dir = Path(os.path.join(self.artifact_dir, DATA_INGESTION_INGESTED_DIR))
-            failed_ingestion_dir = Path(os.path.join(self.artifact_dir, DATA_INGESTION_FAILED_INGESTION_DIR))
+            ingested_data_dir = Path(os.path.join(data_ingestion_artifact_dir, DATA_INGESTION_INGESTED_DIR))
+            failed_ingestion_dir = Path(os.path.join(data_ingestion_artifact_dir, DATA_INGESTION_FAILED_INGESTION_DIR))
             train_file_path= Path(os.path.join(ingested_data_dir, DATA_INGESTION_TRAIN_FILE_PATH))
             test_file_path = Path(os.path.join(ingested_data_dir, DATA_INGESTION_TEST_FILE_PATH))
-
+            meta_data_file_path= Path(os.path.join(data_ingestion_artifact_dir, DATA_INGESTION_METADATA_DIR_NAME,
+                                                    DATA_INGESTION_METADATA_FILE_NAME))
 
             data_ingestion_config = DataIngestionConfig(
                                                     aws_data_source_bucket_name= DATA_SOURCE_BUCKET_NAME,
@@ -42,8 +43,9 @@ class TrainingConfigurationManager:
                                                     test_file_path= test_file_path,
                                                     test_split_ratio= DATA_INGESTION_TEST_SPLIT_RATIO,
                                                     failed_data_ingestion_dir = failed_ingestion_dir,
-                                                    meta_data_file_name=DATA_INGESTION_METADATA_FILE_NAME,
-                                                    data_ingestion_dir_name=DATA_INGESTION_DIR_NAME
+                                                    meta_data_file_path=meta_data_file_path,
+                                                    data_ingestion_dir_name=DATA_INGESTION_DIR_NAME,
+                                                    source_data_file_name=DATA_INGESTION_SOURCE_FILE_NAME
                                                 )
           
             return data_ingestion_config
