7d9bdf86c70609909da93069bd9a97ab4ec84607
anil <anilsai029@gmail.com>
2022-12-12 09:50:17 +0530

    model trainer constants added

---
+++ src/backorder/constants/training_pipeline_config/model_trainer_cosntants.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+
+MODEL_TRAINER_ARTIFACT_DIR_NAME = 'model_trainer'
+MODEL_TRAINER_TRAINED_MODEL_DIR_NAME= "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME="model.pkl"
+MODEL_TRAINER_BASE_ACCURACY = 0.6
+MODEL_TRAINER_MODELS_CONFIG_FILE_PATH = Path(os.path.join('config/model.yaml'))
