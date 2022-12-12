44e9d374d1192311477ff4cbb0ee82ad1b97e9e4
anil <anilsai029@gmail.com>
2022-12-12 13:33:58 +0530

    schema file path updated

--- src/backorder/constants/training_pipeline_config/schema_file_constants.py
+++ src/backorder/constants/training_pipeline_config/schema_file_constants.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 SCHEMA_FILE_TOTAL_NO_COLUMNS = "total_no_of_columns"
 SCHEMA_FILE_TOTAL_NUMERICAL_COLUMNS = "total_numerical_columns"
@@ -9,4 +10,4 @@ SCHEMA_FILE_CATEGORICAL_FEATURES_DATA_TYPES = "Categorical_features_datatypes"
 SCHEMA_FILE_TARGET_COLUMNS_DATA_TYPE = "Target_column"
 SCHEMA_FILE_TARGET_COLUMN_NAME= "Target_column_Name"
 
-SCHEMA_FILE_PATH = Path("src\\backorder\config\schema.yaml")
+SCHEMA_FILE_PATH = Path(os.path.join("config","schema.yaml"))
