ec0ce0ac981d70dc23a74fdc59b639063571cdb2
anil <anilsai029@gmail.com>
2022-12-13 12:10:18 +0530

    updated

--- src/backorder/constants/training_pipeline_config/__init__.py
+++ src/backorder/constants/training_pipeline_config/__init__.py
@@ -2,6 +2,7 @@ from backorder.constants.training_pipeline_config.data_ingestion_constants impor
 from backorder.constants.training_pipeline_config.data_validation_constants  import *
 from backorder.constants.training_pipeline_config.data_transformation_constants import *
 from backorder.constants.training_pipeline_config.model_trainer_cosntants import *
+from backorder.constants.training_pipeline_config.model_evaluation_constants import *
 
 ### common constants for all the components in training pipeline
 ARTIFACT_DIR = "artifact"
