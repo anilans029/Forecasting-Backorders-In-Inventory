24b5bdcc1df8c6729e0e639d9fa23314a9fb4a63
anil <anilsai029@gmail.com>
2022-12-14 21:24:31 +0530

    BackorderData class added

--- src/backorder/ml/model/esitmator.py
+++ src/backorder/ml/model/esitmator.py
@@ -122,7 +122,7 @@ class BackoderModel:
             prediction = self.model.predict(X)
             return prediction
         except Exception as e:
-            logging.info(BackoderModel(e,sys))
+            logging.info(BackorderException(e,sys))
             BackorderException(e,sys)
     
     def predict(self,x):
@@ -130,7 +130,7 @@ class BackoderModel:
             prediction = self.model.predict(x)
             return prediction
         except Exception as e:
-            logging.info(BackoderModel(e,sys))
+            logging.info(BackorderException(e,sys))
             BackorderException(e,sys)
 
     def __repr__(self):
@@ -139,4 +139,95 @@ class BackoderModel:
     def __str__(self):
         return f"{type(self.model).__name__}()"
 
-    
+class BackorderData:
+
+    def __init__(self,
+                national_inv:float,
+                lead_time:float,
+                in_transit_qty:float,
+                forecast_3_month:float,
+                forecast_6_month:float,
+                forecast_9_month:float,
+                sales_1_month:float,
+                sales_3_month:float,
+                sales_6_month:float,
+                sales_9_month:float,
+                min_bank:float,
+                potential_issue:str,
+                pieces_past_due:float,
+                perf_6_month_avg:float,
+                perf_12_month_avg:float,
+                local_bo_qty:float,
+                deck_risk:str,
+                oe_constraint:str,
+                ppap_risk:str,
+                stop_auto_buy:str,
+                rev_stop:str,
+                 ):
+
+        try:
+                self.national_inv= national_inv
+                self.lead_time=lead_time
+                self.in_transit_qty=in_transit_qty
+                self.forecast_3_month=forecast_3_month
+                self.forecast_6_month=forecast_6_month
+                self.forecast_9_month=forecast_9_month
+                self.sales_1_month=sales_1_month
+                self.sales_3_month=sales_3_month
+                self.sales_6_month=sales_6_month
+                self.sales_9_month=sales_9_month
+                self.min_bank=min_bank
+                self.potential_issue=potential_issue
+                self.pieces_past_due=pieces_past_due
+                self.perf_6_month_avg=perf_6_month_avg
+                self.perf_12_month_avg=perf_12_month_avg
+                self.local_bo_qty=local_bo_qty
+                self.deck_risk=deck_risk
+                self.oe_constraint=oe_constraint
+                self.ppap_risk=ppap_risk
+                self.stop_auto_buy=stop_auto_buy
+                self.rev_stop=rev_stop
+            
+        except Exception as e:
+            raise BackorderException(e, sys) from e
+
+
+    def get_backorder_input_data_frame(self):
+        try:
+            backorder_input_dict = self.get_backorder_data_as_dict()
+            return pd.DataFrame(backorder_input_dict)
+
+        except Exception as e:
+            raise BackorderException(e, sys) from e
+
+
+    def get_backorder_data_as_dict(self):
+        try:
+            input_data = {
+               "national_inv": [self.national_inv],
+               "lead_time" :[self.lead_time],
+                "in_transit_qty" :[self.in_transit_qty],
+                "forecast_3_month": [self.forecast_3_month],
+                "forecast_6_month": [self.forecast_6_month],
+                "forecast_9_month": [self.forecast_9_month],
+                "sales_1_month":[self.sales_1_month],
+                "sales_3_month":[self.sales_3_month],
+                "sales_6_month":[self.sales_6_month],
+                "sales_9_month":[self.sales_9_month],
+                "min_bank":[self.min_bank],
+                "potential_issue":[self.potential_issue],
+                "pieces_past_due":[self.pieces_past_due],
+                "perf_6_month_avg":[self.perf_6_month_avg],
+                "perf_12_month_avg":[self.perf_12_month_avg],
+                "local_bo_qty":[self.local_bo_qty],
+                "deck_risk":[self.deck_risk],
+                "oe_constraint":[self.oe_constraint],
+                "ppap_risk":[self.ppap_risk],
+                "stop_auto_buy":[self.stop_auto_buy],
+                "rev_stop":[self.rev_stop],
+                }
+
+            return input_data
+
+        except Exception as e:
+            raise BackorderException(e, sys) 
