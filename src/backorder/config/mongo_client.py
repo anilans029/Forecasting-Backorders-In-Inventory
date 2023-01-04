036041251b32239b434ce60a38405f90990414bb
anilans029 <anilsai029@gmail.com>
2023-01-04 22:30:37 +0530

    minor change

--- src/backorder/config/mongo_client.py
+++ src/backorder/config/mongo_client.py
@@ -5,15 +5,12 @@ import certifi
 
 ca = certifi.where()
  
-
-
 class MongodbClient:
     client = None
 
     def __init__(self, database_name=DATABASE_NAME) -> None:
         if MongodbClient.client is None:
             mongo_db_url = os.getenv(MONGO_DB_URL_ENV_KEY)
-            print(mongo_db_url)
             if mongo_db_url is None:
                 raise Exception(f"Environment key: {MONGO_DB_URL_ENV_KEY} is not set.")
             MongodbClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
