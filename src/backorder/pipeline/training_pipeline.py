15f88bf9acaa759214182b7fa499d3a4c0df2ba7
anil <anilsai029@gmail.com>
2023-01-08 01:05:56 +0530

    minor change to update the meta data file

--- src/backorder/pipeline/training_pipeline.py
+++ src/backorder/pipeline/training_pipeline.py
@@ -7,7 +7,7 @@ from backorder.components import DataIngestion, DataValidation, DataTransformati
 from backorder.exception import BackorderException
 from backorder.logger import logging
 import os,sys
-from backorder.utils import save_artifacts_to_s3_and_clear_local
+from backorder.utils import save_artifacts_to_s3_and_clear_local, update_recent_batch_in_meta_data
 
 
 class TrainingPipeline:
@@ -20,7 +20,7 @@ class TrainingPipeline:
             data_ingestion_config = self.training_config.get_dataingestion_config()
             data_ingestion = DataIngestion(config=data_ingestion_config)
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            return data_ingestion_artifact
+            return data_ingestion_artifact, data_ingestion_config
 
         except Exception as e:
             logging.info(BackorderException(e,sys))
@@ -87,7 +87,7 @@ class TrainingPipeline:
 
     def start(self):
         try:
-            data_ingestion_artifact = self.start_data_ingestion()
+            data_ingestion_artifact,data_ingestion_config = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(
                 data_validation_artifact=data_validation_artifact)
@@ -97,7 +97,11 @@ class TrainingPipeline:
                                                               )
             if model_eval_artifact.is_model_accepted:
                 self.start_model_pusher(model_evaluation_artifact=model_eval_artifact)
+            update_recent_batch_in_meta_data(metadata_file_path=data_ingestion_config.meta_data_file_path,
+                                            newly_downloaded_batches=data_ingestion_artifact.new_batches_timestamps)
             save_artifacts_to_s3_and_clear_local()
+            
+
         except Exception as e:
             logging.info(BackorderException(e,sys))
             raise BackorderException(e, sys)
